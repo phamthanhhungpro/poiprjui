f9ffa20fc617408ccc9f61a6e23347cbcc890859
Pham Thanh Hung <thanhhungsky98@gmail.com>
2024-06-25 10:33:38 +0700

    123

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 const apiUrl = 'http://113.160.187.187:1118/';
 const idFrontEndUrl = 'https://poiidui.vercel.app/';
-const hrmFrontEndUrl = 'https://poihrmui.vercel.app';
+const prjFrontEndUrl = 'https://poiprjui.vercel.app';
 
 export const environment = {
     production: true,
     signInUrl: idFrontEndUrl + 'sign-in',
-    prjFeUrl: hrmFrontEndUrl,
+    prjFeUrl: prjFrontEndUrl,
     apiUrl: apiUrl,
     idApiUrl: apiUrl + 'id/api/',
     idApiUrlWithOutEndding: apiUrl + 'id',
